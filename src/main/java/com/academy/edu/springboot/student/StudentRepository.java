9533b5d5879b26600344516c35cbc883f0794d5e
coalong <minay0904@gmail.com>
2022-06-09 09:55:04 +0900

    + account branch

--- src/main/java/com/academy/edu/springboot/student/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.academy.edu.springboot.student;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    List<Student> findAll();
-}
