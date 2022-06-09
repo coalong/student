6d04f40ca70211c694c2994c93995cbd141ad367
coalong <minay0904@gmail.com>
2022-06-09 11:19:11 +0900

    + student test 실습 3

--- src/test/java/com/academy/edu/springboot/student/StudentControllerTest.java
+++ src/test/java/com/academy/edu/springboot/student/StudentControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,7 +29,7 @@ class StudentControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @SpyBean
     private StudentRepository studentRepository;
 
     @Test
